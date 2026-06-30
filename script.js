9aa76c276635d4473d607068975ae2456f2d88a3
Meenakshi Singh Tanwar <meenakshi28633@gmail.com>
2026-06-30 21:03:48 +0530

    Add cursor glow and 3D card tilt

--- script.js
+++ script.js
@@ -116,5 +116,42 @@ function deleteBlog(id){
         showBlogs();
     }
 }
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const rotateY = ((x / rect.width) - 0.5) * 20;
+        const rotateX = ((y / rect.height) - 0.5) * -20;
+
+        card.style.transform =
+            `perspective(1000px)
+             rotateX(${rotateX}deg)
+             rotateY(${rotateY}deg)
+             scale(1.05)`;
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+            "perspective(1000px) rotateX(0) rotateY(0) scale(1)";
+
+    });
+
+});
+
 
 
